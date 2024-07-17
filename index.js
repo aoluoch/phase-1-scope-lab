2429b2cfee1e00278c1a9a24ec66b7a9695942e7
Amos Oluoch <aoluoch271@gmail.com>
2024-07-17 11:37:11 +0300

    scope

--- index.js
+++ index.js
@@ -1,22 +1,18 @@
-// Write your solution in this file!
-var customerName = 'bob';
+var customerName="bob";
 
-// Function to uppercase the global customerName variable
 function upperCaseCustomerName() {
-  customerName = customerName.toUpperCase();
+    return customerName=customerName.toUpperCase();
 }
 
-// Function to declare a global variable bestCustomer and assign it 'not bob'
-function setBestCustomer() {
-  window.bestCustomer = 'not bob';
+function setBestCustomer(){
+     bestCustomer="not bob"
 }
 
-
-function overwriteBestCustomer() {
-  bestCustomer = 'maybe bob';
+function overwriteBestCustomer(){
+    bestCustomer="maybe bob"
 }
 
-// Declare a constant in global scope and assign it an initial value
-const leastFavoriteCustomer = 'alice';
-
-
+const leastFavoriteCustomer="Jesse"
+function changeLeastFavoriteCustomer(){
+    leastFavoriteCustomer="Josh";
+}
